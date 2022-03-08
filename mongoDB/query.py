1a551b40244215bb7bfa046e6ae38673e74b6730
Toby Wong <toby@wong.com.au>
2022-03-08 23:56:48 +1100

    finished several user functions for db

--- mongoDB/query.py
+++ mongoDB/query.py
@@ -28,8 +28,55 @@ def newUser(db, user):
             "lastName": user["lastName"],
             "password": user["password"],
             "phoneNumber": user["phoneNumber"],
-            "username": user["username"]
+            "username": user["username"],
+            "permissions": {
+                "consumer": False,
+                "provider": False,
+                "admin": False
+            }
         })
+        return True
+    else:
+        return False
+
+# Change password for user
+def changePassword(db, email, newPassword):
+    if emailExists(db, email):
+        db.users.update_one({"email": email}, {"$set": {"password": newPassword}})
+        return True
+    else:
+        return False
+
+# need to update function to include verification for new email/phone
+# def updateDetails(db, email, newDetails):
+#     if emailExists(db, email):
+#         for key in newDetails:
+#             db.users.update_one({"email": email}, {"$set": {key: newDetails[key]}})
+#         return True
+#     else:
+#         return False
+
+# Delete user from database
+def deleteUser(db, email):
+    if emailExists(db, email):
+        db.users.delete_one({"email": email})
+        return True
+    else:
+        return False
+
+# Get permissions for user
+def getPermissions(db, email):
+    if emailExists(db, email):
+        user = db.users.find_one({"email": email})
+        return user["permissions"]
+    else:
+        return False
+
+# Update permissions for user
+def updatePermissions(db, email, permissions):
+    if emailExists(db, email):
+        for key in permissions:
+            db.users.update_one({"email": email}, {"$set": {"permissions"[key]: permissions[key]}})
         return True
     else:
         return False
--- mongoDB/test_query.py
+++ mongoDB/test_query.py
@@ -1,12 +1,12 @@
-import query
+from query import *
 
 def testInitDB():
-    db = query.initDB()
+    db = initDB()
     assert db is not None, "Database should exist"
     return db
 
 def testEmailExists(db):
-    user = query.emailExists(db, "y0unggil10919@gmail.com")
+    user = emailExists(db, "y0unggil10919@gmail.com")
     assert user is not None, "User should exist"
 
 def testNewUser(db):
@@ -18,11 +18,31 @@ def testNewUser(db):
         "phoneNumber": "0404111222",
         "username": "y0unggil0919"
     }
-    query.newUser(db, testUser)
+    newUser(db, testUser)
     user = db.users.find_one(testUser)
     assert user is not None, "User should exist"
 
-    if __name__ == "__main__":
-        db = testInitDB()
-        testEmailExists(db)
-        testNewUser(db)
+def testChangePassword(db):
+    testEmail = "y0unggil0919@gmail.com"
+    testPassword = "password"
+    assert db.users.find_one({"email": email})["password"] != testPassword, "Test password should not be the same as existing password"
+    changePassword(db, testEmail, testPassword)
+    assert db.users.find_one({"email": email})["password"] == testPassword, "Current password is not updated to test password"
+
+# def testUpdateDetails(db):
+#     return
+
+def testDeleteUser(db):
+    return
+
+def testGetPermissions(db):
+    return
+
+def testUpdatePermissions(db):
+    return
+
+
+if __name__ == "__main__":
+    db = testInitDB()
+    testEmailExists(db)
+    testNewUser(db)
