0952030a986495da002119f258bf9947ca7ee529
Tom Tak <y0ung-gil@hanmail.net>
2022-03-09 14:30:38 +1100

    Set up frontend

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import { AppBar } from "@mui/material";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return <AppBar position="static">Header</AppBar>;
+};
+
+export default Header;
