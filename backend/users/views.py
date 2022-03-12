776a0aad66491d8b75789ac22b0997a442da4dc2
Toby Wong <toby@wong.com.au>
2022-03-13 01:41:05 +1100

    changed user deletion to deactivation

--- backend/users/views.py
+++ backend/users/views.py
@@ -14,7 +14,9 @@ class RemoveUserView(GenericAPIView):
         username = request.data.get('username')
         user = CustomUser.objects.get(username=username)
         if user is not None:
-            user.delete()
+            # user.delete()
+            user.is_active = False
             return Response({'message': 'User deleted'}, status=status.HTTP_204_NO_CONTENT)
         else:
             return Response({'message': 'User not deleted'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
