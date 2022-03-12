78fb9109a8e476de13cbbfd4143610b36d4b021d
toby-wong <toby@wong.com.au>
2022-03-12 19:07:52 +1100

    updated email template

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,6 +58,17 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'parkit.bmg@gmail.com'
 EMAIL_HOST_PASSWORD = 'XiX4C5N2w!GaQe!tCx'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+    }
+
+]
 
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': "%m/%d/%Y %I:%M%P", 
