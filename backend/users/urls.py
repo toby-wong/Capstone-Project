b38d61b6ce86a07d52682351f42ffe0deeaddc5b
Toby Wong <toby@wong.com.au>
2022-04-17 23:58:40 +1000

    stopped tracking via gitlfs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,16 +1,7 @@
 from django.urls import include, path
-from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
-from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 from users.views import *
 
 urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls')),    
-    path('auth/register/', include('dj_rest_auth.registration.urls')),
-    path('registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view(),), # Needs to be defined before the registration path
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password/reset/confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('delete/user', RemoveUserView.as_view(), name='remove_user'), # redundant if we use HTTP method on /auth/user?
 
 
